db1f21d21cd9349c727176751f9c59263c7ff333
gauravsoni97 <gauravsoni8414@gmail.com>
2024-03-15 17:58:40 +0530

    project setup start

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import ProductCard from "../components/ProductCard";
+import Header from "../components/Header";
+import Sidebar from "../components/Sidebar";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <>
+      <Header />
 
-export default Home
+      <Sidebar />
+
+      <div className="p-4 sm:ml-64">
+        <div className="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14">
+          <div className="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-4 mb-4">
+            <ProductCard />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
