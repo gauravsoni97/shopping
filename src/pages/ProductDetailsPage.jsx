232471275b20b47569795d92cf05af1442181dff
gauravsoni97 <gauravsoni8414@gmail.com>
2024-03-18 12:05:16 +0530

    product details logic set

--- src/pages/ProductDetailsPage.jsx
+++ src/pages/ProductDetailsPage.jsx
@@ -4,11 +4,19 @@ import { useProductsContext } from "../context/ProductListContext";
 
 const ProductDetailsPage = () => {
   const { products } = useProductsContext();
-  let { id } = useParams();
 
-  const res = products.find((ele) => ele.id === id);
+  const { id } = useParams();
 
-  console.log("response", res);
+  console.log("res", id);
+  const [selectedProduct, setSetSelectedProduct] = useState([]);
+  console.log("selectedProducts", selectedProduct);
+
+  useEffect(() => {
+    console.log("products", products, id);
+    const res = products.find((ele) => ele.id - 1 === parseInt(id));
+
+    setSetSelectedProduct(res);
+  }, [products]);
 
   return <div>ProductDetailsPage</div>;
 };
