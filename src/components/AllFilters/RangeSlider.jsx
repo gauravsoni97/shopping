74552709126e41792da1ffd437bc8fe3536f3b90
gauravsoni97 <gauravsoni8414@gmail.com>
2024-04-01 13:31:03 +0530

    price range filter set

--- src/components/AllFilters/RangeSlider.jsx
+++ src/components/AllFilters/RangeSlider.jsx
@@ -2,25 +2,34 @@ import * as React from "react";
 import { useState } from "react";
 import Box from "@mui/material/Box";
 import Slider from "@mui/material/Slider";
+import { useProductsContext } from "../../context/ProductListContext";
 
 function valuetext(value) {
   return `${value}`;
 }
 
 const RangeSlider = () => {
-  const [value, setValue] = useState([20, 37]);
+  const { products, setProducts } = useProductsContext();
+
+  const [value, setValue] = useState([0, 2000]);
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
+    let ProductInRange = products.filter(
+      (ele) => ele.price <= value[1] && ele.price > value[0]
+    );
+    setProducts(ProductInRange);
   };
 
   return (
     <Box sx={{ width: "90%" }}>
       <Slider
-        getAriaLabel={() => "Temperature range"}
+        getAriaLabel={() => "Price range"}
         value={value}
         onChange={handleChange}
         valueLabelDisplay="auto"
+        min={0}
+        max={2000}
         getAriaValueText={valuetext}
       />
     </Box>
