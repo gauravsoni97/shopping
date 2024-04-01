a747b4b9e50b047a7df508f55910e3b4f835768c
gauravsoni97 <gauravsoni8414@gmail.com>
2024-04-01 14:05:32 +0530

    view moew route set

--- src/components/AllFilters/StarRating.jsx
+++ src/components/AllFilters/StarRating.jsx
@@ -2,9 +2,25 @@ import * as React from "react";
 import Box from "@mui/material/Box";
 import Rating from "@mui/material/Rating";
 import Typography from "@mui/material/Typography";
+import { useProductsContext } from "../../context/ProductListContext";
 
 const StarRating = () => {
   const [value, setValue] = React.useState(2);
+
+  const { products, setProducts } = useProductsContext();
+
+  console.log(
+    "products filtered",
+    products.filter((ele) => Math.floor(ele.rating) <= value)
+  );
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+    const ProductInRange = products.filter(
+      (ele) => Math.floor(ele.rating) <= value
+    );
+    setProducts(ProductInRange);
+  };
 
   return (
     <Box
@@ -12,13 +28,7 @@ const StarRating = () => {
         "& > legend": { mt: 2 },
       }}
     >
-      <Rating
-        name="simple-controlled"
-        value={value}
-        onChange={(event, newValue) => {
-          setValue(newValue);
-        }}
-      />
+      <Rating name="simple-controlled" value={value} onChange={handleChange} />
     </Box>
   );
 };
